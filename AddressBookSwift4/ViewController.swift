437b2294b0b00c8f6b68dec314065d0d6788ac2b
idrisAd <idrisadrien@hotmail.fr>
2017-10-25 10:16:59 +0200

    Initial Commit

---
+++ AddressBookSwift4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AddressBookSwift4
+//
+//  Created by Idris Adrien on 25/10/2017.
+//  Copyright © 2017 Idris Adrien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
